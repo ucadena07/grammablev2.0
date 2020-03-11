5d9e1ff2de58d9b9dba38b74c1bff20499adba98
Ulises Cadena <ucadena07@gmail.com>
2020-03-11 18:54:10 -0500

    validating grams

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -24,4 +24,10 @@ RSpec.describe GramsController, type: :controller do
       expect(gram.message).to eq('Hello!')
     end
   end
+
+  it "should properly deal with validation errors" do
+    post :create, params: { gram: { message: ''} }
+    expect(response).to have_http_status(:unprocessable_entity)
+    expect(Gram.count).to eq 0
+  end
 end
