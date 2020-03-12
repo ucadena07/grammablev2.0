6905b5c6b7a4d3a03ecf84949ffdd33d405b62e1
Ulises Cadena <ucadena07@gmail.com>
2020-03-12 09:39:52 -0500

    destroying grams

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -34,6 +34,13 @@ class GramsController < ApplicationController
     end
   end
 
+  def destroy 
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    @gram.destroy
+    redirect_to root_path
+  end
+
   def show
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,6 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
+  describe "grams#destroy action" do
+    it "should allow a user to destroy a gram" do
+      gram = FactoryBot.create(:gram)
+      delete :destroy, params: { id: gram.id }
+      expect(response).to redirect_to root_path
+      gram = Gram.find_by_id(gram.id)
+      expect(gram).to eq nil
+    end
+
+    it "should return a 404 message if we cannot find a gram with the id that is spicified" do
+      delete :destroy, params: { id: 'spaceduck'}
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
   describe "grams#update action" do
     it "should allow users to successfullyt update a gram" do
       gram = FactoryBot.create(:gram, message: 'Initial Value')
