58738f67c3eb3f193c8bde77937861982f6bb03d
Ulises Cadena <ucadena07@gmail.com>
2020-03-13 09:56:57 -0500

    secure edit and destroy

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,5 +1,5 @@
 class GramsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   def index 
   end
 
@@ -19,11 +19,13 @@ class GramsController < ApplicationController
   def edit 
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
   end
 
   def update 
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
 
     @gram.update_attributes(gram_params)
 
@@ -37,6 +39,7 @@ class GramsController < ApplicationController
   def destroy 
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
     @gram.destroy
     redirect_to root_path
   end
@@ -48,11 +51,12 @@ class GramsController < ApplicationController
 
   private 
 
+  def render_not_found(status=:not_found)
+  render plain: "#{status.to_s.titleize} :(", status: status
+  end
+
   def gram_params
     params.require(:gram).permit(:message)
   end
 
-  def render_not_found
-    render plain: 'Not Found :(', status: :not_found
-  end
 end
